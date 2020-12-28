559be8f040302bbdd1d51eca7708df0934edb4d2
dahee kim <ekekekgml@naver.com>
2020-12-28 18:17:06 +0900

    9020 골드바흐의 추측 - 에라토스테네스의 체 적용

--- 2021/9020_A.cpp
+++ 2021/9020_A.cpp
@@ -4,21 +4,23 @@ using namespace std;
 int T;
 int arr[10001] = { 0, }; // 0 = prime number, 1 = composite number
 
-
+//에라토스테네스의 체
 void isPrimeNum() {
-	for (int i = 3; i < 10001; i++) {
-		for (int j = 2; j < i; j++) {
-			if (i % j == 0) arr[i] = 1;
+	for(int i = 2; i<10001; i++){
+		for (int j = i*2; j < 10001; j += i) {
+			arr[j] = 1;
 		}
 	}
 }
 
 int Answer(int num) {
+	//절반부터 탐색
 	int length = num / 2;
 	for (int i = 0; i < length; i++) {
 		int temp1 = length - i;
 		int temp2 = num - temp1;
 
+		// 배열에 접근한 뒤, 둘 다 소수이면 출력
 		if (arr[temp1] == 0) {
 			if (arr[temp2] == 0) {
 				cout << temp1 << ' ' << temp2 << '\n';
@@ -40,5 +42,6 @@ int main() {
 		Answer(num);
 	}
 
+	return 0;
 
 }
